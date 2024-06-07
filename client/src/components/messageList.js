1c5e9a3e2e22099253e81081cd76cab066020303
Jose Riaño <jriano@konecta-group.com>
2024-06-07 07:52:44 -0500

    Cambios para docker imagen

--- client/src/components/messageList.js
+++ client/src/components/messageList.js
@@ -15,6 +15,24 @@ const MessageList = ({ messages, currentUser }) => {
         }
     };
 
+    const formatTime = (timestamp) => {
+        const date = new Date(timestamp);
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+    
+        // Agrega un cero inicial si el número es menor que 10
+        const formattedDay = day < 10 ? `0${day}` : day;
+        const formattedMonth = month < 10 ? `0${month}` : month;
+        const formattedHours = hours < 10 ? `0${hours}` : hours;
+        const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+        const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+    
+        return `${formattedDay}/${formattedMonth}/${year} ${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+    };
     return (
         <div className="message-list-container" ref={messageListRef}>
             <h3>Mensajes:</h3>
@@ -25,7 +43,7 @@ const MessageList = ({ messages, currentUser }) => {
                         className={`message ${message.sender === currentUser ? 'sent' : 'received'}`}
                     >
                         <p><strong>{message.sender}</strong>: {message.message}</p>
-                        <small>{message.timestamp}</small>
+                        <small>{formatTime(message.timestamp)}</small>
                     </li>
                 ))}
             </ul>
@@ -34,3 +52,5 @@ const MessageList = ({ messages, currentUser }) => {
 };
 
 export default MessageList;
+
+
